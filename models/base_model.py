f50168a8a5212b47db273f0a4fb7d6041570d49e
jhojan <jhojan_perlaza123>
2022-06-30 21:22:14 -0500

    [UPDATED] file base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,41 +3,33 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    id = str(uuid.uuid4())
-    created_at = datetime.now() 
-    updated_at = datetime.now()
-    
-    def save(self):
-        self.updated_at = datetime.now()
+
+    def __init__(self, *args, **kwargs):
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                if key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                if key != "__class__" and key != "updated_at" and key != "created_at":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Returns a string representation"""
-        self.id = BaseModel.id
-        self.created_at = BaseModel.created_at
-        self.updated_at = self.updated_at
         string_str = "[BaseModel] "
         string_str += "({}) {}".format(self.id, self.__dict__)
-        return string_str   
+        return string_str
+
+    def save(self):
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         self.created_at = self.created_at.isoformat('T','auto')
         self.updated_at = self.updated_at.isoformat('T','auto')
         self.__dict__["__class__"] = BaseModel.__name__
         return self.__dict__
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            if key == "created_at":
-                self.created_at = datetime.fromisoformat(value)
-            if key == "updated_at":
-                self.updated_at = datetime.fromisoformat(value)
-            if key == "updated_at":
-                self.updated_at = value
-            if key == "name":
-                self.name = value
-            if key == "my_number":
-                self.my_number = value
