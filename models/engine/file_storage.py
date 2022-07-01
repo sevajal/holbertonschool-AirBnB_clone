68903976c5f0a278d58747020c058e1f0688b986
jhojan <jhojan_perlaza123>
2022-07-01 15:54:23 -0500

    [ADD] engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 
 import json
 import os.path
-from models.base_model import BaseModel
+
 
 class FileStorage:
     __file_path = "file.json"
@@ -11,15 +11,16 @@ class FileStorage:
     def all(self):
         return self.__objects
 
-    def new(self, obj):
-        new_obj = str(obj.__name__) + "." + str(obj.id)
-        self.__objects[new_obj] = obj
+    def new(self, obj): 
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj.__dict__
 
     def save(self):
         with open(self.__file_path, mode="w", encoding="utf-8") as file:
-            file.write(json.dumps(self.__objects))
+            self.__objects = json.dumps(self.__objects, sort_keys=True, default=str)
+            file.write(self.__objects)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
+        if os.path.exists("file.json"):
             with open(self.__file_path, mode="r", encoding="utf-8") as file:
-                self.__object = json.loads(file.read())
+                self.__objects = json.loads(file.read())
