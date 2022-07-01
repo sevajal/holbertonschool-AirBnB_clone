cc123799b87cb8a51251f4c64fdd6bed3a14da4f
jhojan <jhojan_perlaza123>
2022-07-01 16:23:17 -0500

    [UPDATED] file file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
 
     def new(self, obj): 
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.__dict__
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         with open(self.__file_path, mode="w", encoding="utf-8") as file:
@@ -21,6 +21,6 @@ class FileStorage:
             file.write(self.__objects)
 
     def reload(self):
-        if os.path.exists("file.json"):
+        if os.path.exists(self.__file_path):
             with open(self.__file_path, mode="r", encoding="utf-8") as file:
                 self.__objects = json.loads(file.read())
