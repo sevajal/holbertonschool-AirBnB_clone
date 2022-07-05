e3c279460b6d94548b63a165476274ecfd6ee2ab
jhojan <jhojan_perlaza123>
2022-07-05 12:09:44 -0500

    [UPDATED] file base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
+"""
+BaseModel class that defines all common
+attributes/methods for other classes
+"""
 
 import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
+    """ BaseModel class """
 
     def __init__(self, *args, **kwargs):
-
+        """ Initial methods """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
                     self.created_at = datetime.fromisoformat(value)
                 if key == "updated_at":
                     self.updated_at = datetime.fromisoformat(value)
-                if key != "__class__" and key != "updated_at" and key != "created_at":
+                if key not in ["__class__", "updated_at", "created_at"]:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -23,16 +29,19 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
+        """ return the string representation of the class instance """
         string_str = "[" + self.__class__.__name__ + "] "
         string_str += "({}) {}".format(self.id, self.__dict__)
         return string_str
 
     def save(self):
+        """ serializes __objects to the JSON file """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        self.created_at = self.created_at.isoformat('T','auto')
-        self.updated_at = self.updated_at.isoformat('T','auto')
+        """ dictionary containing all keys/values of __dict__ """
+        self.created_at = self.created_at.isoformat('T', 'auto')
+        self.updated_at = self.updated_at.isoformat('T', 'auto')
         self.__dict__["__class__"] = self.__class__.__name__
         return self.__dict__
