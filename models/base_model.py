cf88424b7e4b2afe69b812a4d310ae45cf1d185f
jhojan <jhojan_perlaza123>
2022-07-05 11:53:01 -0500

    [UṔDATED] FILES

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 
 import uuid
 from datetime import datetime
-from venv import create
 from models import storage
 
 class BaseModel:
@@ -21,10 +20,10 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
 
     def __str__(self):
-        string_str = "[BaseModel] "
+        string_str = "[" + self.__class__.__name__ + "] "
         string_str += "({}) {}".format(self.id, self.__dict__)
         return string_str
 
@@ -33,10 +32,7 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        for key, value in self.__dict__.items():
-            if key == "created_at":
-                value = self.created_at.isoformat('T','auto')
-            if key == "updated_at":
-                value = self.updated_at.isoformat('T','auto')
-        self.__dict__["__class__"] = BaseModel.__name__
+        self.created_at = self.created_at.isoformat('T','auto')
+        self.updated_at = self.updated_at.isoformat('T','auto')
+        self.__dict__["__class__"] = self.__class__.__name__
         return self.__dict__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,10 @@
 #! /usr/bin/python3
 
+from hashlib import new
 import json
 import os.path
+
+from tomlkit import value
 
 
 class FileStorage:
@@ -13,7 +16,8 @@ class FileStorage:
 
     def new(self, obj): 
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
+        self.__objects[key] = obj
+
 
     def save(self):
         with open(self.__file_path, mode="w", encoding="utf-8") as file:
