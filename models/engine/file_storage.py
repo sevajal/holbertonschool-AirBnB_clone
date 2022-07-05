191666c2d1e465d6a27044e72c8287c66125566a
jhojan <jhojan_perlaza123>
2022-07-05 16:58:31 -0500

    [UPDATED] file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,43 @@
 #! /usr/bin/python3
+"""
+Serializes instances to a JSON file and
+deserializes JSON file to instances
+"""
 
 import json
 import os.path
 
 
 class FileStorage:
+    """ class FileStorage """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ returns the dictionary __objects """
         return self.__objects
 
-    def new(self, obj): 
+    def new(self, obj):
+        """ sets in __objects the obj with the new key """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
-
+        self.__objects[key] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file """
+        dictionary = {}
+
+        for key, value in self.__objects.items():
+            dictionary[key] = value.to_dict()
         with open(self.__file_path, mode="w", encoding="utf-8") as file:
-            self.__objects =  json.dump (self.__objects, file)
+            json.dump(dictionary, file)
 
     def reload(self):
+        """ Deserializes the JSON file to """
+        from models.base_model import BaseModel
+        dict_class = {'BaseModel': BaseModel}
+
         if os.path.exists(self.__file_path):
             with open(self.__file_path, mode="r", encoding="utf-8") as file:
-                self.__objects = json.load(file)
+                for key, value in json.load(file).items():
+                    self.__objects[key] = dict_class[value['__class__']](
+                        **value)
