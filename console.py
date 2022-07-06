14827866fea46e2bb58d8dbc5101a972ef2a5ba4
jhojan <jhojan_perlaza123>
2022-07-05 21:31:21 -0500

    [UPDATED] console

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 contains the entry point of
 the command interpreter
 """
+
 import cmd
 from models.base_model import BaseModel
 from models import storage
@@ -11,6 +12,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """ class command interpreter """
     prompt = '(hbnb) '
+    dict_class = {"BaseModel": BaseModel}
 
     def split_args(self, line):
         """ Split arguments by spaces """
@@ -18,6 +20,11 @@ class HBNBCommand(cmd.Cmd):
         for arg in line.split(" "):
             list.append(arg)
         return list
+
+    def get_class(self, list_args):
+        for key, value in self.dict_class.items():
+            if key == list_args[0]:
+                return value
 
     def do_EOF(self, arg):
         'EOF command to exit the program'
@@ -36,40 +43,43 @@ class HBNBCommand(cmd.Cmd):
         if not arg:
             print("** class name missing **")
             return
-        if arg != "BaseModel":
+        args = self.split_args(arg)
+        class_name = self.get_class(args)
+        if class_name == None:
             print("** class doesn't exist **")
             return
-        new_instance = BaseModel()
+        new_instance = class_name()
         new_instance.save()
-        storage.reload()
         print(new_instance.id)
 
     def do_show(self, arg):
         'Prints the string representation of an instance(class/id)'
-        args = self.split_args(arg)
-        if not args[0]:
+        if not arg:
             print("** class name missing **")
             return
-        if args[0] != "BaseModel":
+        args = self.split_args(arg)
+        class_name = self.get_class(args)
+        if class_name == None:
             print("** class doesn't exist **")
             return
         if len(args) == 1:
             print("** instance id missing **")
             return
         dir_class = storage.all()
-        for key, value in dir_class.items():
+        for key, objects in dir_class.items():
             if key == args[0] + '.' + args[1]:
-                print(BaseModel(**value))
+                print(objects)
                 return
         print("** no instance found **")
 
     def do_destroy(self, arg):
         'command to destroy an instance'
-        args = self.split_args(arg)
-        if not args[0]:
+        if not arg:
             print("** class name missing **")
             return
-        if args[0] != "BaseModel":
+        args = self.split_args(arg)
+        class_name = self.get_class(args)
+        if class_name == None:
             print("** class doesn't exist **")
             return
         if len(args) == 1:
@@ -85,22 +95,29 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         'command to show all instances'
-        if not arg or arg == "BaseModel":
+        class_name = ""
+        args = self.split_args(arg)
+        for key in self.dict_class.keys():
+            if key == args[0]:
+                class_name = key
+
+        if not arg or arg == class_name:
             string_all = []
             dir_class = storage.all()
             for key, value in dir_class.items():
-                string_all.append(str(BaseModel(**value)))
+                string_all.append(str(value))
             print(string_all)
             return
         print("** class doesn't exist **")
 
     def do_update(self, arg):
         'command to update an instance'
-        args = self.split_args(arg)
-        if not args[0]:
+        if not arg:
             print("** class name missing **")
             return
-        if args[0] != "BaseModel":
+        args = self.split_args(arg)
+        class_name = self.get_class(args)
+        if class_name == None:
             print("** class doesn't exist **")
             return
         if len(args) == 1:
@@ -114,7 +131,8 @@ class HBNBCommand(cmd.Cmd):
         dir_class = storage.all()
         for key, value in dir_class.items():
             if key == args[0] + '.' + args[1]:
-                value[args[2]] = args[3].strip('"')
+                dict_class = value.__dict__
+                dict_class[args[2]] = args[3].strip('"')
                 storage.save()
                 return
         print("** no instance found **")
