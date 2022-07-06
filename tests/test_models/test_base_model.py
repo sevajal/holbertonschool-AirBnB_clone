8ba1436d915808e6d552e6aa7801bc09d155cdbc
sevajal <sevajal@yahoo.com>
2022-07-06 18:20:17 -0500

    [TEST] base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel Class
-"""
-
-
 import unittest
 from models.base_model import BaseModel
 
 
 class Test_base(unittest.TestCase):
-    """Class Test for BaseModel"""
 
     def test_uuid(self):
-        """Test the uuid"""
         bm1 = BaseModel()
         bm2 = BaseModel()
         self.assertIsInstance(bm1, BaseModel)
@@ -20,14 +14,12 @@ class Test_base(unittest.TestCase):
         self.assertIsInstance(bm1.id, str)
 
     def test_datetime(self):
-        """Test the datatime attributes"""
         bm1 = BaseModel()
         self.assertTrue(hasattr(bm1, "created_at"))
         self.assertTrue(hasattr(bm1, "updated_at"))
         self.assertNotEqual(bm1.created_at, bm1.updated_at)
 
     def test_add_attributes(self):
-        """Test new attributes"""
         bm1 = BaseModel()
         bm1.name = "Sebas"
         bm1.number = 90
