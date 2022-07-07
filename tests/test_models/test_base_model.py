9482518916596e9bd480ece9ee5ca36bc62045d4
jhojan <jhojan_perlaza123>
2022-07-06 19:47:57 -0500

    [UPDATED] test FILE

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -52,6 +52,39 @@ class BaseModelTests(unittest.TestCase):
         self.assertIsInstance(bm1.created_at, datetime)
         self.assertIsInstance(bm1.updated_at, datetime)
 
+    my_model = BaseModel()
+
+    def testBaseModel1(self):
+        """ Test for a BaseModel instance """
+
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+        self.my_model.save()
+        my_model_json = self.my_model.to_dict()
+
+        self.assertEqual(self.my_model.name, my_model_json['name'])
+        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
+        self.assertEqual('BaseModel', my_model_json['__class__'])
+        self.assertEqual(self.my_model.id, my_model_json['id'])
+
+    def testSave(self):
+        """ Test for a BaseModel instance """
+        self.my_model.first_name = "First"
+        self.my_model.save()
+
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+
+        first_dict = self.my_model.to_dict()
+
+        self.my_model.first_name = "Second"
+        self.my_model.save()
+        sec_dict = self.my_model.to_dict()
+
+        self.assertEqual(first_dict['created_at'], sec_dict['created_at'])
+        self.assertNotEqual(first_dict['updated_at'], sec_dict['updated_at'])
+
 
 if __name__ == '__main__':
     unittest.main()
