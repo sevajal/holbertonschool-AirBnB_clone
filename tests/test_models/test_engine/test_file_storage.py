c6359dd9fb169fe0af808ae60dc9e7a7fc2d5a3f
jhojan <jhojan_perlaza123>
2022-07-06 19:28:50 -0500

    [UPDATED] test FILE

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,10 +5,15 @@ Unittest for FileStorage Class
 
 import unittest
 from models.engine.file_storage import FileStorage
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+import os
+import json
 
 class Test_FileStorage(unittest.TestCase):
     """Class Test for FileStorage"""
+    base = BaseModel()
 
     @classmethod
     def setUpClass(cls):
@@ -21,6 +26,19 @@ class Test_FileStorage(unittest.TestCase):
         self.assertEqual(file, {})
         self.assertIsInstance(file, dict)
 
+    def testClassInstance(self):
+        """ Check instance """
+        self.assertIsInstance(storage, FileStorage)
+
+    def testStoreBaseModel(self):
+        """ Test save and reload functions """
+        self.base.new_name = "new_instance"
+        self.base.save()
+        basemodel_dict = self.base.to_dict()
+        all_objs = storage.all()
+
+        key = basemodel_dict['_class_'] + "." + basemodel_dict['id']
+        self.assertEqual(key in all_objs, False)
 
 if __name__ == '__main__':
     unittest.main()
